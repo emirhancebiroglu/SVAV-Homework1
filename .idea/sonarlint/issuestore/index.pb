d11a2dd4d97908bdbda9a696a821a71327b8513d
emirhancebiroglu <152030621+emirhancebiroglu@users.noreply.github.com>
2024-04-21 18:42:43 +0300

    Initial commit

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,7 @@
+
+]
+-src/test/java/org/example/CalculatorTest.java,d\d\ddf9d3f44eebbd90607904161f037b4aeea53ec5
+X
+(src/test/java/csv/divisionTestValues.csv,1\8\18b3c6bfe294f502fab18778a51fd9c2fb0c6489
+7
+pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
---
+++ src/test/java/org/example/CalculatorTest.java
@@ -0,0 +1,72 @@
+/*
+    Student number: B211202065
+    Student name: Emirhan Cebiroğlu
+    Course name: Software Verification and Validation
+    Homework Info: Homework1 / 2023-2024 Spring Semester
+    Github Repository: https://github.com/emirhancebiroglu/SVAV-Homework
+* */
+
+
+package org.example;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = {"/csv/divisionTestValues.csv"})
+    void testDivisionParameterized(float dividend, float divisor, float expected){
+        assertEquals(expected, Calculator.divide(dividend, divisor));
+    }
+
+    @Test
+    void testDivision1() {
+        assertEquals(5, Calculator.divide(10, 2));
+    }
+
+    @Test
+    void testDivision2() {
+        assertEquals(2.5, Calculator.divide(10, 4));
+    }
+
+    @Test
+    void testDivision3() {
+        assertEquals(5, Calculator.divide(12.5f, 2.5f));
+    }
+
+    @Test
+    void testDivision4() {
+        assertEquals(4, Calculator.divide(10, 2.5f));
+    }
+
+    @Test
+    void testDivision5() {
+        assertEquals(2.5f, Calculator.divide(12.5f, 5));
+    }
+
+    @Test
+    void testDivision6() {
+        Exception exception = assertThrows(
+                IllegalArgumentException.class,
+                () -> Calculator.divide(12.5f, 0),
+                "IllegalArgumentException expected."
+        );
+
+        // Optional. To check whether the error messages match.
+        assertEquals("Illegal Argument Exception.", exception.getMessage());
+    }
+}
